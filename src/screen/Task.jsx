d31ed4e82f8c4ca7c27603e076ea058079a49d8e
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2025-02-26 16:40:36 +0100

    update

--- src/screen/Task.jsx
+++ src/screen/Task.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import filtericon from "../assets/filtericon.svg";
 
 function Task() {
   return (
     <div className='techdash'>
       <h2 className='techdashgreet'>Task</h2>
+      <div className='tasksearch'>
+        <input type='text' placeholder='Search here...' />
+        <button className='filterbtn'>
+          <img src={filtericon} alt='' srcset='' />
+        </button>
+      </div>
     </div>
   );
 }
