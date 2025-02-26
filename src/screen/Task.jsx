f1554e7cadd94644ee3d2a2c07b9c01485c2bd87
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2025-02-26 17:28:56 +0100

    update

--- src/screen/Task.jsx
+++ src/screen/Task.jsx
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import filtericon from "../assets/filtericon.svg";
 
+export const taskbtnlist = [
+  {
+    id: 0,
+    name: "All",
+  },
+  {
+    id: 1,
+    name: "Pending",
+  },
+  {
+    id: 0,
+    name: "In Progress",
+  },
+  {
+    id: 0,
+    name: "Completed",
+  },
+];
+
 function Task() {
+  const [activenav, setActivena] = useState("All");
   return (
     <div className='techdash'>
       <h2 className='techdashgreet'>Task</h2>
       <div className='tasksearch'>
         <input type='text' placeholder='Search here...' />
         <button className='filterbtn'>
-          <img src={filtericon} alt='' srcset='' />
+          <img src={filtericon} alt='' srcSet='' />
         </button>
+      </div>
+      <div className='tasknav'>
+        {taskbtnlist.map((tasktype) => (
+          <button
+            key={tasktype.id}
+            onClick={() => setActivena(tasktype.name)}
+            className={`tasknavitem ${
+              activenav === tasktype.name && "tasktypeactive"
+            }`}
+          >
+            {tasktype.name}
+          </button>
+        ))}
       </div>
     </div>
   );
