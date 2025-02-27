b6ff9bcbf39776314589dd806e14d11e05e456dc
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2025-02-27 22:34:46 +0100

    Update task_details.jsx

--- src/screen/task_details.jsx
+++ src/screen/task_details.jsx
@@ -15,6 +15,27 @@ function TaskDetails() {
       <div className=''>Pending</div>
       <div className='thsp'></div>
       <p>Task info</p>
+      <div className="">
+        <p>Date booked</p>
+        <p>Sun, Nov 25, 2025</p>
+        <div className="divider"></div>
+        <p>Location</p>
+        <p>VI Lagos</p>
+        <div className="divider"></div>
+        <p>Price breakdown</p>
+        <div className="pricebreakitem">
+            <p>Hourly rate:</p>
+            <p>N15,000/hr</p>
+        </div>
+        <div className="pricebreakitem">
+            <p>Trust/support fee:</p>
+            <p>N9,500</p>
+        </div>
+        <div className="pricebreakitem">
+            <p>Total fee:</p>
+            <p>N25,500</p>
+        </div>
+      </div>
     </div>
   );
 }
