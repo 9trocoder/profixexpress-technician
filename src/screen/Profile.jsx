8bb8175af25e99925548660466b46e5743a93f60
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2025-03-17 00:27:07 +0100

    update

--- src/screen/Profile.jsx
+++ src/screen/Profile.jsx
@@ -13,7 +13,7 @@ const Profile = () => {
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
   const [technician, setTechnician] = useState(null);
-   const [online, setOnline] = useState(false);
+  const [online, setOnline] = useState(false);
 
   useEffect(() => {
     const user = auth.currentUser;
@@ -34,10 +34,7 @@ const Profile = () => {
         setOnline(techData.online);
       }
     });
-
-  
   }, [navigate]);
-
 
   return (
     <>
@@ -94,13 +91,29 @@ const Profile = () => {
             </div>
           </div>
         )}
-        <h1 className="bookingtitle">Profile</h1>
-        <div className="thetechprof">
-            <img src={profileimg} alt="" />
+        <h1 className='bookingtitle'>Profile</h1>
+        <div className="thegap"></div>
+        <div className='thetechprof'>
+          <img className='theprofimg' src={profileimg} alt='' />
+          <p className='theprofname'>{technician?.fullName}</p>
+          <p className='theprofemail'>{technician?.email}</p>
+          <div className='theprostarate'>
+            <svg
+              width='20'
+              height='20'
+              viewBox='0 0 15 15'
+              fill='#feca57'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                d='M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z'
+                fill='#feca57'
+              ></path>
+            </svg>
             <p className=''>
-              {technician?.fullName }
+              4.5 (108 review) <span>see all review</span>
             </p>
-            <p>{technician?.email}</p>
+          </div>
         </div>
       </div>
     </>
